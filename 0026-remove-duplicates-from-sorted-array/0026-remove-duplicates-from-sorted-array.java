ce46517ded9063faaeaeade953520de8ead24300
Z <zhanarbek.a7@gmail.com>
2025-04-11 03:48:02 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,18 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 0) {
-            return 0;
-        }
- 
-        int i = 0;
- 
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[j] != nums[i]) {
-                i++;
-                nums[i] = nums[j];
+        int index = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i]!=nums[index]){
+                nums[index++] = nums[i];
             }
         }
- 
-        return i + 1;
+        return index;
     }
 }
