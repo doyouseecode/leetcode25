d4d25a35670a57746d20bef6ce9237c0ee410966
Z <zhanarbek.a7@gmail.com>
2025-05-24 05:35:37 -0500

    Time: 1 ms (68.98%), Space: 45.4 MB (58.95%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -1,27 +1,21 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-    // Check for null matrix
-    if (mat == null || mat.length == 0 || mat[0].length == 0) {
-        throw new IllegalArgumentException("Matrix cannot be null or empty.");
-    }
-    
-    int m = mat.length, n = mat[0].length;
-    if (m * n != r * c) {
-        return mat; // Reshape not possible, return original matrix
-    }
-
-    int[][] res = new int[r][c];
-    int k = 0, l = 0;
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            res[k][l] = mat[i][j];
-            l++;
-            if (l == c) {
-                k++;
-                l = 0;
-            }
+        if (mat.length * mat[0].length != r * c) {
+            return mat;
         }
-    }
-    return res;
+            int[][] res = new int[r][c];
+            int k = 0 , l = 0;
+            for(int i = 0; i < mat.length; i++){
+                for(int j = 0; j < mat[0].length; j++){
+                    res[k][l] = mat[i][j];
+                    l++;
+                    if(l==c){
+                        k++;
+                        l=0;
+                    }
+                }
+
+            }
+            return res;
     }
 }
